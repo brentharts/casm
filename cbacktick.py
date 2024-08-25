72efc3a607d645b17e7d1fa00beab21be9e23d01
brent hartshorn <brenthartshorn@proton.me>
2024-08-25 10:58:01 -0700

    compile linux and debug print registers

--- cbacktick.py
+++ cbacktick.py
@@ -11,6 +11,86 @@ sys.path.append('./holypycparser')
 import holyguacamole as guaca
 print(guaca)
 
+def decomp(o):
+	objdump_output = subprocess.check_output(["riscv64-linux-gnu-objdump", "-d", o]).decode("utf-8")
+	asm = []
+	sects = {}
+	info = {'sections':sects, 'asm':asm}
+	sect  = None
+	current_section = None
+	for ln in objdump_output.splitlines():
+		if '--verbose' in sys.argv: print(ln)
+		if ln.startswith("Disassembly of section"):
+			current_section = ln.split()[-1]
+			sect = {}
+			sects[current_section]=sect
+			continue
+
+		elif ln.strip().endswith(':'):  ## label
+			asm.append(ln[ln.index(' '):])
+			print(asm[-1])
+		elif ':' in ln:
+			if ln.startswith('./') and 'file format' in ln: continue
+			a = ln[ ln.index(':')+1 : ].strip().split()[1:]
+			print(a)
+			a = ' '.join(a)
+			asm.append(a)
+
+		if current_section:
+			if ln.startswith('  ') and ':' in ln:
+				ops = None
+				if '<' in ln:
+					ln, comment = ln.split('<')
+				if '#' in ln:
+					ln, comment2 = ln.split('#')
+
+				if ln.count(':')==1:
+					mem, ln = ln.split(':')
+
+				a = ln.split()
+				if len(a)==3:
+					mem2, inst, ops = a
+				elif len(a)==2: ## some inst have no ops, like ret,mret,nop..
+					mem2, inst = a
+				else:
+					raise RuntimeError(len(a))
+				#print(a)
+				if ops:
+					for b in ops.split(','):
+						if b in guaca.REGS:
+							if b not in sect: sect[b] = {'count':0,'asm':[]}
+							sect[b]['count'] += 1
+							sect[b]['asm'].append(ln)
+						## only checking the first operand
+						break
+
+	return info
+
+
+def print_regs(register_usage):
+	for section, registers in register_usage.items():
+		print(section)
+		for reg in registers:
+			if reg in guaca.reg_colors:
+				print('\033[%sm' % guaca.reg_colors[reg], end='')
+			elif reg.startswith('a'):
+				if reg in guaca.A_COLORS:
+					clr = '48;5;%s' % guaca.A_COLORS[reg]
+				else:
+					clr = '30;44'
+				print('\033[%sm' % clr, end='')
+			elif reg.startswith('s'):
+				if reg in guaca.S_COLORS:
+					clr = '48;5;%s' % guaca.S_COLORS[reg]
+				else:
+					clr = '30;43'
+				print('\033[%sm' % clr, end='')
+			print('	%s : %s' %(reg, registers[reg]['count']), end='')
+			print('\033[0m')
+			if registers[reg]['count'] <= 10:
+				for asm in registers[reg]['asm']:
+					print('		: %s' %(asm))
+
 def mklinux():
 	## https://risc-v-getting-started-guide.readthedocs.io/en/latest/linux-qemu.html
 	if not os.path.isfile('/usr/bin/riscv64-linux-gnu-gcc'):
@@ -20,13 +100,21 @@ def mklinux():
 		cmd = 'git clone --depth 1 https://github.com/torvalds/linux.git'.split()
 		print(cmd)
 		subprocess.check_call(cmd)
-	cmd = 'make V=1 ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- defconfig'.split()
-	print(cmd)
-	subprocess.check_call(cmd, cwd='./linux')
-	cmd = 'make V=1 ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu-'.split()
-	print(cmd)
-	subprocess.check_call(cmd, cwd='./linux')
 
+	vmlinux = './linux/vmlinux'
+	if not os.path.isfile(vmlinux) or '--rebuild' in sys.argv:
+		cmd = 'make V=1 ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu- defconfig'.split()
+		print(cmd)
+		subprocess.check_call(cmd, cwd='./linux')
+		cmd = 'make V=1 ARCH=riscv CROSS_COMPILE=riscv64-linux-gnu-'.split()
+		print(cmd)
+		subprocess.check_call(cmd, cwd='./linux')
+
+	subprocess.check_call(['ls', '-lh', './linux/vmlinux'])
+
+	info = decomp( './linux/virt/kvm/kvm_main.o' )
+	print_regs(info['sections'])
+	guaca.print_asm( '\n'.join(info['asm']) )
 
 if __name__ == '__main__':
 	out = None
@@ -49,7 +137,6 @@ if __name__ == '__main__':
 				print('saving:', out)
 				open(out,'w').write(s)
 
-	if '--linux' in sys.argv:
-		mklinux()
-	elif not c:
+	if not c:
 		print('no .c input files')
+		mklinux()
